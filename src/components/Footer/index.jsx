241beec105935577eacad3976091be94fa47d657
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2023-10-28 14:00:20 -0300

    bug de largura corrigido

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import "./styles.css"
 const index = () => {
   return (
     <footer>
-        <p>Desenvolvido por <a href="https://github.com/FelipeGPellegrini">Felipe Pellegrini</a></p>
+        <p>Desenvolvido por <a target="_blank" href="https://github.com/FelipeGPellegrini" rel="noreferrer">Felipe Pellegrini</a></p>
     </footer>
   )
 }
